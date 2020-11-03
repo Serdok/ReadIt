e10209e0de4afb35539c78214fb7821af5f91ba7
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2020-11-03 15:23:02 +0100

    :tada: Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ReadIt';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'https://angular.io', 10),
+      new Article('React', 'https://reactjs.org', 5),
+      new Article('Vue', 'https://vuejs.org', 1)
+    ];
+  }
+
+  addArticle(newTitle: HTMLInputElement, newLink: HTMLInputElement): boolean {
+    this.articles.push(new Article(newTitle.value, newLink.value));
+    newTitle.value = '';
+    newLink.value = '';
+    return false;
+  }
+
+  sortArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 }
